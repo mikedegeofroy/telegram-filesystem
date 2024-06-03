f4b5ed20e84cc05d148b13ef9692c95139e116e4
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-06-03 13:23:45 +0000

    Minor changes to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,49 @@
 #include "./application/TelegramFileSystemService.h"
 #include "./application/models/Directory.h"
 
-int main(int argc, char* argv[]) {
-  std::shared_ptr<TelegramFileSystemService> fs(new TelegramFileSystemService);
+#define FUSE_USE_VERSION 26
+#include <fuse.h>
 
-  auto root = fs->getEntitiesInPath("fs");
+#include <iostream>
 
-  if (std::shared_ptr<Directory> d = std::dynamic_pointer_cast<Directory>(root);
-      d != nullptr) {
-    for (auto& entity : d->entities) {
-      std::cout << entity->name << " : " << entity->path << "\n";
-      if (std::shared_ptr<Directory> dir =
-              std::dynamic_pointer_cast<Directory>(entity);
-          d != nullptr) {
-        for (auto& file : dir->entities) {
-          std::cout << file->name << ", " << file->path << ", " << file->size
-                    << "\n";
-        }
-      }
-    }
+class MyFuseOperations {
+ public:
+  static std::shared_ptr<TelegramFileSystemService> fs;
+
+  static void initialize(std::shared_ptr<TelegramFileSystemService> service) {
+    fs = service;
   }
 
-  return 0;
+  static int myfs_getattr(const char *path, struct stat *stbuf) {
+    stbuf->st_mode = S_IFDIR | 00400;
+    return 0;
+  }
+
+  static int myfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+                          off_t offset, struct fuse_file_info *fi) {
+
+    auto root = fs->getEntitiesInPath("fs");
+
+    if (std::shared_ptr<Directory> d =
+            std::dynamic_pointer_cast<Directory>(root);
+        d != nullptr) {
+      for (auto &entity : d->entities) {
+        filler(buf, entity->name.c_str(), NULL, 0);
+      }
+    }
+
+    return 0;
+  }
+};
+
+static struct fuse_operations myOperations = {
+    .getattr = MyFuseOperations::myfs_getattr,
+    .readdir = MyFuseOperations::myfs_readdir};
+
+int main(int argc, char *argv[]) {
+  std::shared_ptr<TelegramFileSystemService> fs(new TelegramFileSystemService);
+
+  MyFuseOperations::initialize(fs);
+
+  return fuse_main(argc, argv, &myOperations, NULL);
 }
