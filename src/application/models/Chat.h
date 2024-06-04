1faf78d6280c5762ed8e28d4d8a0887820157487
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-06-04 07:37:45 +0300

    Joined the teleram-integration with the fuse-integration, but still suffering with multithreading
    Co-authored-by: Etozheraf <Etozheraf@users.noreply.github.com>

--- src/application/models/Chat.h
+++ src/application/models/Chat.h
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 
-#include "Message.h";
+#include "Message.h"
 
 struct Chat {
   std::string name;
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #define FUSE_USE_VERSION 26
-#include"presentation/FuseOperations.h"
+#include "presentation/FuseOperations.h"
 
-static struct fuse_operations myOperations = {.getattr = myfs_getattr,
-                                              .readdir = myfs_readdir};
+static struct fuse_operations myOperations = {};
 
 int main(int argc, char *argv[]) {
+  myOperations.init = myfs_init;
+  myOperations.destroy = myfs_destroy;
+
+  myOperations.getattr = myfs_getattr;
+  myOperations.readdir = myfs_readdir;
+
   myOperations.open = myfs_open;
   myOperations.read = myfs_read;
   myOperations.release = myfs_release;
-  
+
   return fuse_main(argc, argv, &myOperations, 0);
 }
